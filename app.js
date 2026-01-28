70d52359844827e1bd8754332ed5a96b1b76aacc
Brahmakosh Team <brahmakoshtech@gmail.com>
2026-01-29 01:44:52 +0530

    New changes

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ app.use(cors({
 app.use((req, res, next) => {
   res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
   res.setHeader('Cross-Origin-Embedder-Policy', 'unsafe-none');
+  res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
   next();
 });
 
